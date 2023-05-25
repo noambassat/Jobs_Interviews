0b5dae9cda55616ebe8ecc18f9351d96def6f609
noambassat <noambassat92@gmail.com>
2023-05-25 16:32:35 +0300

    fizzbuzz

--- OtherQuests/FizzBuzz.py
+++ OtherQuests/FizzBuzz.py
@@ -1,3 +1,8 @@
+
+dic = {3:"Fizz",5:"Buzz"}
 for i in range(1,51):
-    if(i%3==0): print("Fizz")
-    if(i%5==0): print("Buzz")
+    print_str = ""
+    for k in dic.keys():
+        if(i%k ==0): print_str +=dic[k]
+    if(print_str==""): print(i)
+    else: print(print_str)
