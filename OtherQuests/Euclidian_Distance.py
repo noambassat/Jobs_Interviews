95e879d1728cc609b92ff3a813ebbab2977421ca
noambassat <noambassat92@gmail.com>
2023-05-28 14:58:23 +0300

    project

--- OtherQuests/Euclidian_Distance.py
+++ OtherQuests/Euclidian_Distance.py
@@ -1,87 +1,92 @@
-#
-# !pip install scikit-learn
-# !pip install imbalanced-learn
-
-from sklearn.ensemble import RandomForestClassifier, AdaBoostClassifier
-from imblearn.over_sampling import SMOTE
+from sklearn.ensemble import RandomForestClassifier
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import classification_report
+import pandas as pd
+import numpy as np
 
-# Prepare your dataset with the features (X) and labels (y)
+# Define the number of customers
+num_customers = 1000
 
-# Assuming X and y are already prepared
+# Define the classes and their respective sizes
+minor_classes = [0, 1, 2]
+major_class = 3
+minor_class_size = int(num_customers * 0.025)
+major_class_size = num_customers - (len(minor_classes) * minor_class_size)
 
+# Create an empty DataFrame to store the customer data
+columns = ["age", "income", "credit_score", "class"]
+data = pd.DataFrame(columns=columns)
 
-X = [[1,2,3],[5,6,7],[1,7,3],[12,3,7],[11,3,8],[23,5,9],[6,8,3],[55,44,12],[45,2,7],[4,2,7],[20,7,3],[79,4,6],[23,44,92],[9,2,9],[7,2,7],[6,6,6]]
-y = [1,1,1,2,2,2,0,0,0,0,0,0,0,0,0,0]
+# Generate data for the minor classes
+for class_label in minor_classes:
+    age = np.random.randint(18, 65, size=minor_class_size)
+    income = np.random.randint(20000, 80000, size=minor_class_size)
+    credit_score = np.random.randint(300, 850, size=minor_class_size)
+    class_data = pd.DataFrame({"age": age, "income": income, "credit_score": credit_score, "class": class_label})
+    data = pd.concat([data, class_data], ignore_index=True)
 
-# Split the dataset into training and testing sets
+# Generate data for the major class
+age = np.random.randint(18, 65, size=major_class_size)
+income = np.random.randint(20000, 80000, size=major_class_size)
+credit_score = np.random.randint(300, 850, size=major_class_size)
+class_data = pd.DataFrame({"age": age, "income": income, "credit_score": credit_score, "class": major_class})
+data = pd.concat([data, class_data], ignore_index=True)
+
+# Shuffle the data
+data = data.sample(frac=1, random_state=42).reset_index(drop=True)
+
+# Separate the features (X) and the target variable (y)
+X = data.drop("class", axis=1)
+y = data["class"]
+
+# Split the data into training and testing sets
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
-print(X_train)
-print(y_train)
-# Apply SMOTE to oversample the small classes
-smote = SMOTE(random_state=42)
-X_train_resampled, y_train_resampled = smote.fit_resample(X_train, y_train)
-print(X_train_resampled)
-print(y_train_resampled)
 
-# Initialize the random forest classifier as the base estimator
-base_estimator = RandomForestClassifier()
+# Create a DataFrame with the features and target variable
+train_data = pd.concat([X_train, y_train], axis=1)
 
-# Initialize the AdaBoost classifier with the random forest as the base estimator
-boosted_rf = AdaBoostClassifier(base_estimator=base_estimator, random_state=42)
+# Split the training data into subsets for Bagging Random Forest
+subset_size = int(len(train_data) * 0.25)  # Size of each subset
+n_subsets = len(minor_classes)  # Number of subsets
 
-# Train the boosted random forest on the resampled training data
-boosted_rf.fit(X_train_resampled, y_train_resampled)
+subsets = []
 
-# Make predictions on the testing data
-y_pred = boosted_rf.predict(X_test)
+# Create subsets with oversampled minor classes and random samples from the major class
+for _ in range(n_subsets):
+    # Oversample minority classes without replacement
+    minority_data = train_data[train_data["class"].isin(minor_classes)]
+    oversampled_minority = minority_data.groupby("class").apply(lambda x: x.sample(subset_size, replace=False))
+
+    # Randomly sample from the major class
+    majority_data = train_data[~train_data["class"].isin(minor_classes)]
+    majority_sampled = majority_data.sample(n=subset_size, random_state=42)
+
+    # Combine the oversampled minority and randomly sampled majority data
+    subset_data = pd.concat([oversampled_minority, majority_sampled], axis=0)
+
+    subsets.append(subset_data)
+
+# Train individual Random Forest models on each subset
+models = []
+for subset in subsets:
+    X_subset = subset.drop("class", axis=1)
+    y_subset = subset["class"]
+    model = RandomForestClassifier(n_estimators=100, random_state=42)
+    model.fit(X_subset, y_subset)
+    models.append(model)
+
+# Make predictions on the test set using each model
+predictions = []
+for model in models:
+    y_pred = model.predict(X_test)
+    predictions.append(y_pred)
+
+# Combine the predictions using majority voting
+ensemble_predictions = []
+for i in range(len(X_test)):
+    preds = [predictions[j][i] for j in range(n_subsets)]
+    ensemble_pred = max(set(preds), key=preds.count)
+    ensemble_predictions.append(ensemble_pred)
 
 # Print classification report
-print(classification_report(y_test, y_pred))
-
-
-#
-# In this updated code, we import the SMOTE class from the imblearn.over_sampling module. After splitting the dataset into training and testing sets, we apply the fit_resample method of SMOTE to oversample the small classes in the training data. The resulting resampled training set, X_train_resampled and y_train_resampled, is then used for training the boosted random forest classifier.
-#
-# The remaining parts of the code, such as initializing the random forest and AdaBoost classifiers, making predictions, and printing the classification report, remain the same as before.
-#
-# Make sure you have the imbalanced-learn library installed (pip install imbalanced-learn) to use the SMOTE class.
-#
-# Adjust the code according to your specific dataset and problem requirements, including any necessary preprocessing steps or modifications.
-
-
-# Get the feature importances from the Random Forest classifier
-predictor_names = ["age", "income", "education"]
-
-
-feature_importances = boosted_rf.estimators_[0].feature_importances_
-
-# Create a dictionary to store the predictor names and their importances
-predictor_importances = {predictor_names[i]: importance for i, importance in enumerate(feature_importances)}
-
-# Print the significant predictors and their coefficients
-print("Significant Predictors:")
-for predictor, importance in predictor_importances.items():
-    print(f"{predictor}: {importance}")
-
-# Sort the predictors based on their importance (in descending order)
-sorted_predictors = sorted(predictor_importances, key=predictor_importances.get, reverse=True)
-
-# Determine the strong predictors (top n predictors with highest importances)
-n = 5  # Number of top predictors to consider
-strong_predictors = sorted_predictors[:n]
-
-# Print the strong predictors
-print("\nStrong Predictors:")
-for predictor in strong_predictors:
-    importance = predictor_importances[predictor]
-    print(f"{predictor}: {importance}")
-
-# In the above code, the feature importances obtained from the Random Forest base estimator are stored in the feature_importances variable. These importances are associated with the corresponding predictor names using a dictionary called predictor_importances.
-#
-# The code then proceeds to print all the significant predictors along with their coefficients. Next, the predictors are sorted based on their importance in descending order. The variable n represents the number of top predictors to consider as strong predictors. In this case, it is set to 5.
-#
-# The top n predictors with the highest importances are stored in the strong_predictors list. Finally, the code prints the strong predictors along with their respective coefficients.
-#
-# You can modify the code as per your dataset and requirements. Remember to replace X_train and y_train with your actual training data, and predictor_names with your list of predictor names or column labels.
+print(classification_report(y_test, ensemble_predictions))
